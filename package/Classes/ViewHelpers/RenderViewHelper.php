24d94a8759e8fe76e30ce4735b06b660e92ee9a3
mneuhaus <mneuhaus@54391d9a-7e4f-4783-837b-884f03fbcd44>
2010-02-07 01:01:17 +0000

    Replaced the ObjectAccessors inside the ModelFormViewhelper which return the Form Widgets with a Temporary kinda Stupid Viewhelper to circumvent the automatic Escaping of ObjectAccessor Nodes which got recently into Fluid.
    Will be obsolete when/if Feature #6389 gets done
    
    git-svn-id: https://svn.typo3.org/FLOW3/Packages/Admin/trunk@3811 54391d9a-7e4f-4783-837b-884f03fbcd44

---
+++ package/Classes/ViewHelpers/RenderViewHelper.php
@@ -0,0 +1,44 @@
+<?php
+ 
+namespace F3\Admin\ViewHelpers;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Fluid".                      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * @version $Id: ForViewHelper.php 3346 2009-10-22 17:26:10Z k-fish $
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @api
+ * @scope prototype
+ */
+class RenderViewHelper extends \F3\Fluid\Core\ViewHelper\AbstractViewHelper {
+	/**
+	 *
+	 * @param object $value
+	 * @return string Rendered string
+	 * @author Marc Neuhaus <apocalip@gmail.com>
+	 * @api
+	 */
+	public function render($value) {
+		return $value;
+	}
+}
+
+?>
