960800515c357754129c49f2d40c56a11b527cd7
mneuhaus <mneuhaus@54391d9a-7e4f-4783-837b-884f03fbcd44>
2010-06-16 13:26:50 +0000

    Started to modularize the Templates and added Bulk Actions and Filters to the Table
    git-svn-id: https://svn.typo3.org/FLOW3/Packages/Admin/trunk@4546 54391d9a-7e4f-4783-837b-884f03fbcd44

--- package/Resources/Public/js/main.js
+++ package/Resources/Public/js/main.js
@@ -4,47 +4,6 @@ jQuery(document).ready(function(){
 		jQuery(this).find("tbody tr:odd").addClass("odd");
 	});
 	
-	// Form Enhancements
-	jQuery(".f-date").datepicker();
-	//jQuery(".f-datetimerange").daterangepicker(); 
-	jQuery(".f-spinner").spinner();
-	jQuery(".f-slider").slider({
-		slide: function(event, ui) {
-			jQuery(this).siblings("input").val(ui.value);
-			jQuery(this).siblings(".f-slider-value").text(ui.value);
-		}
-	});
-	
-	jQuery(".f-multiselect").css("width","400px").css("height","200px").multiselect();
-	jQuery(".available connected-list").css("height","177px");
-	
-	jQuery("select.f-autosuggest").autoSuggest();
-	jQuery(".f-filtercombo").sexyCombo();
-	jQuery('.f-multifile').MultiFile();
-	jQuery('.f-autoexpand').elastic();
-	jQuery('.f-datejs').datejs();
-	
-	jQuery('.f-fullrte').ckeditor();
-	jQuery('.f-markdown').markItUp(markdownSettings);
-	jQuery('.f-textile').markItUp(textileSettings);
-	jQuery('.f-wiki').markItUp(wikiSettings);	
-	jQuery('.f-bbcode').markItUp(bbcodeSettings);
-	jQuery('.f-dotclear').markItUp(dotclearSettings);
-	jQuery('.f-texy').markItUp(texySettings);
-	
-	jQuery('#navigation a, .subnav a, a.ui-button, .hotnav').css("position","relative").hotnav();
-	
-	jQuery("input[type='text']:first").focus();
-	
-	jQuery(document).bind('keydown', {combi:'esc'}, function(){
-		jQuery("input").blur();
-    });
-	
-	// jQuery(".sortable").after(jQuery("<a href='#' class='ui-button'>Add one more</a>").click(function(){
-	//     jQuery(".sortable").append(jQuery(".sortable > div:first").clone());
-	//     return false;
-	// }));
-	
 	jQuery(".sortable").sortable();
 	jQuery(".sortable").each(function(){
 		var container = jQuery(this);
@@ -56,7 +15,7 @@ jQuery(document).ready(function(){
 		container.after(button);
 	});
 	
-	
+	/*
 	jQuery(".ui-table").tablesorter({widgets: ['zebra']});
 	
 	jQuery(".ui-table th").click(function(){	
@@ -66,5 +25,48 @@ jQuery(document).ready(function(){
 			jQuery(this).find("tbody tr:odd").addClass("odd");
 		});
 	});
-	
+	*/
+
+   jQuery(".ux-select-all").change(function(){
+       jQuery(".ux-select-row").attr('checked', jQuery(this).attr('checked'));
+   });
 });
+
+
+	/*
+	// Form Enhancements
+	jQuery(".f-date").datepicker();
+	//jQuery(".f-datetimerange").daterangepicker();
+	jQuery(".f-spinner").spinner();
+	jQuery(".f-slider").slider({
+		slide: function(event, ui) {
+			jQuery(this).siblings("input").val(ui.value);
+			jQuery(this).siblings(".f-slider-value").text(ui.value);
+		}
+	});
+	*/
+	//jQuery(".f-multiselect").css("width","400px").css("height","200px").multiselect();
+    /*
+	jQuery(".available connected-list").css("height","177px");
+	jQuery("select.f-autosuggest").autoSuggest();
+	jQuery(".f-filtercombo").sexyCombo();
+	jQuery('.f-multifile').MultiFile();
+	jQuery('.f-autoexpand').elastic();
+	jQuery('.f-datejs').datejs();
+
+	jQuery('.f-fullrte').ckeditor();
+	jQuery('.f-markdown').markItUp(markdownSettings);
+	jQuery('.f-textile').markItUp(textileSettings);
+	jQuery('.f-wiki').markItUp(wikiSettings);
+	jQuery('.f-bbcode').markItUp(bbcodeSettings);
+	jQuery('.f-dotclear').markItUp(dotclearSettings);
+	jQuery('.f-texy').markItUp(texySettings);
+	jQuery('#navigation a, .subnav a, a.ui-button, .hotnav').css("position","relative").hotnav();
+
+	jQuery("input[type='text']:first").focus();
+
+	jQuery(document).bind('keydown', {combi:'esc'}, function(){
+		jQuery("input").blur();
+    });
+
+	*/
