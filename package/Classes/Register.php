2bb4f1cb059822a9c29bca7fe49f06405dda8045
mneuhaus <mneuhaus@54391d9a-7e4f-4783-837b-884f03fbcd44>
2010-06-16 13:22:30 +0000

    added an add Function
    git-svn-id: https://svn.typo3.org/FLOW3/Packages/Admin/trunk@4538 54391d9a-7e4f-4783-837b-884f03fbcd44

--- package/Classes/Register.php
+++ package/Classes/Register.php
@@ -13,11 +13,25 @@ class Register {
     }
     
 	static function get($name){
-        return self::$container[$name];
+        return isset(self::$container[$name]) ? self::$container[$name] : null;
     }
 	
 	static function has($name){
 		return array_key_exists($name,self::$container);
+	}
+
+    static function add($name,$key = null,$value = null){
+        if(isset(self::$container[$name]) && !is_array(self::$container[$name])){
+            self::$container[$name] = array();
+        }
+        if($key === null)
+            self::$container[$name][] = $value;
+        else
+            self::$container[$name][$key] = $value;
+    }
+
+	static function remove($name){
+		unset(self::$container[$name]);
 	}
 }
 
