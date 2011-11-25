aaaaf5167d9c4d7ee497eab3f8328de256f428c1
Marc Neuhaus <mneuhaus@famelo.com>
2011-11-25 10:52:30 +0100

    Added some Helptext for DateJs

--- Resources/Public/dateJsInput/jquery.dateJsInput.js
+++ Resources/Public/dateJsInput/jquery.dateJsInput.js
@@ -1,11 +1,11 @@
 ;(function ( $, window, document, undefined ) {
 	var pluginName = 'dateJsInput';
 	var defaults = {
-			messages: ["Nope", "Keep Trying", "Nadda", "Sorry", "No one\'s home", "Arg", "Bummer", "Faux pas", "Whoops", "Snafu", "Blunder"],
+			messages: ["Nope", "Keep Trying", "Nadda", "Sorry", "Arg", "Bummer", "Faux pas", "Whoops", "Snafu", "Blunder"],
 			date_result: '<span class="date-result help-inline"></span>',
 			format: "dddd, MMMM dd, yyyy h:mm:ss tt"
 		};
-	
+
 	function Plugin( element, options ) {
 		this.element = jQuery(element);
 		
@@ -20,6 +20,7 @@
 	Plugin.prototype.init = function () {
 		var name = this.element.attr("name");
 		var hidden = jQuery('<input type="hidden" class="date-result"/>').attr("name", name);
+		this.element.after("<div class='help-text help-block'>Here’s a few suggestions to try…<table><tbody><tr><td>today</td><td>tomorrow</td><td>July 2008</td><td>next friday</td><td>last April</td></tr><tr><td>2004.08.07</td><td>6/4/2005</td><td>8:15 PM</td><td>22:30:45</td><td>+5years</td></tr></tbody></table></div>");
 		this.element.after(hidden);
 		this.element.after(this.options.date_result);
 		this.element.data("options", this.options).attr("autocomplete", "off");
@@ -40,7 +41,8 @@
 					result.removeClass("important").addClass("success").text(date.toString(format));
 					hidden.val(date.toString(format));
 				} else {
-					result.removeClass("success").addClass("important").text(options.messages[Math.round(options.messages.length * Math.random())] + "...");
+//						result.removeClass("success").addClass("important").text(options.messages[Math.round(options.messages.length * Math.random())] + "...");
+						result.removeClass("success").addClass("important").text("Sorry, i didn't understand...");
 					hidden.val();
 				}
 			} else {
